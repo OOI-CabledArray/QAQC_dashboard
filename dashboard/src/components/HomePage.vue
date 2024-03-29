30ae2bb5d00b6f11b5a8877eaa0f0f4a2e5ec3bb
jdduprey <jduprey@uw.edu>
2024-03-29 10:10:55 -0700

    lead edit

--- dashboard/src/components/HomePage.vue
+++ dashboard/src/components/HomePage.vue
@@ -2,7 +2,7 @@
   <b-jumbotron
     class="mt-5"
     header="RCA Data QA/QC Dashboard"
-    lead="Updated daily at 0400 - 0800 PST">
+    lead="Updated daily at 0800 PST">
   </b-jumbotron>
 </template>
 
