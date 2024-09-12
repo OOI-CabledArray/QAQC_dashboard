a2327c2d142e11ba2a7b3dc224321a8971ae03e2
jdduprey <jduprey@uw.edu>
2024-09-12 09:27:46 -0700

    pending in dropdown

--- dashboard/src/store/index.js
+++ dashboard/src/store/index.js
@@ -222,6 +222,7 @@ export default new Vuex.Store({
           { key: 'Watch', value: 'Watchlist' },
           { key: 'Plotting', value: 'Plotting Issues' },
           { key: 'Harvest', value: 'Data Harvest Issues' },
+          { key: 'Pending', value: 'Pending'}
         ],
       },
 
