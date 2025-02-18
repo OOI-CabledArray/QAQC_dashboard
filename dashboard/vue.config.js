c4573f94be649e8948bbef44297d043c51e7119d
jdduprey <jduprey@uw.edu>
2025-02-18 14:06:13 -0800

    local debug config

--- dashboard/vue.config.js
+++ dashboard/vue.config.js
@@ -1,11 +1,16 @@
-const crypto = require('crypto');
-
+// not sure why this workaround was here, jupyterlab is not currently part of the front end?
+//const crypto = require('crypto');
+// module.exports = {
+//   configureWebpack: () => {
+//     // Workaround for loaders using "md4" by default,
+//     // which is not supported in FIPS-compliant OpenSSL
+//     // See https://github.com/jupyterlab/jupyterlab/issues/11248
+//     const cryptoOrigCreateHash = crypto.createHash;
+//     crypto.createHash = (algorithm) => cryptoOrigCreateHash(algorithm === 'md4' ? 'sha256' : algorithm); // eslint-disable-line no-eval
+//   },
+// };
 module.exports = {
-  configureWebpack: () => {
-    // Workaround for loaders using "md4" by default,
-    // which is not supported in FIPS-compliant OpenSSL
-    // See https://github.com/jupyterlab/jupyterlab/issues/11248
-    const cryptoOrigCreateHash = crypto.createHash;
-    crypto.createHash = (algorithm) => cryptoOrigCreateHash(algorithm === 'md4' ? 'sha256' : algorithm); // eslint-disable-line no-eval
-  },
-};
+  configureWebpack: {
+    devtool: 'source-map'
+  }
+}
